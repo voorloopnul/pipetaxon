fff88562503e1ab66b49f862b8b15bb9ad379bb0
Ricardo Pascal <voorloop@gmail.com>
2018-11-26 22:26:32 -0200

    remove unused code

--- www/models.py
+++
@@ -1,8 +0,0 @@
-from django.core.mail import EmailMultiAlternatives
-
-
-def send_email_with_token(to, token):
-    subject, from_email, to = 'New token to access PipeTaxon API', 'noreply@mg.voorloop.com', to
-    text_content = 'Hey,\nHere is your new access token for PipeTaxon: {0}\n\nRegards,\nRicardo Pascal'.format(token.key)
-    msg = EmailMultiAlternatives(subject, text_content, from_email, [to], reply_to=('pipetaxon@voorloop.com',))
-    msg.send()
