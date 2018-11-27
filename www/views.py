0da8646e456400ba0c6c11dee167495445fa2bc4
Ricardo Pascal <voorloop@gmail.com>
2018-11-26 22:06:13 -0200

    move send_mail logic from views.py to forms.py

--- www/views.py
+++ www/views.py
@@ -1,9 +1,6 @@
-import uuid
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
-from rest_framework.authtoken.models import Token
 from .forms import GetTokenForm
-from .models import send_email_with_token
 
 User = get_user_model()
 
@@ -13,21 +10,11 @@ def features(request):
 
 
 def request_token(request):
+    msg = None
     form = GetTokenForm()
     if request.POST:
         form = GetTokenForm(request.POST)
         if form.is_valid():
-            email = form.cleaned_data['email']
-
-            try:
-                user = User.objects.get(username=email)
-            except User.DoesNotExist:
-                user = User.objects.create(username=email, password=uuid.uuid4().hex)
-
-            token, created = Token.objects.get_or_create(user=user)
-            if not created:
-                token.delete()
-                token = Token.objects.create(user=user)
-            send_email_with_token(email, token)
-
-    return render(request, 'www/register.html', {'form': form})
+            form.create_token()
+            msg = "Token sent to {0}".format(form.cleaned_data['email'])
+    return render(request, 'www/register.html', {'form': form, 'msg': msg})
