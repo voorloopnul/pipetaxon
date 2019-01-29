6d73794ef4c7ad278fe00f5150f3527f3c00b7e5
Ricardo Pascal <voorloop@gmail.com>
2019-01-29 15:41:47 -0200

    remove www app from pipetaxon foss version

--- www/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from www.views import features, request_token
-
-urlpatterns = [
-    path('features/', features, name='site-features'),
-    path('register/', request_token, name='site-register'),
-]
